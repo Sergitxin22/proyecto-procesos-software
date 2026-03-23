7d4b0d2ebea913868aeaafa9c15cc7099cdfe7f9
aroa-im <aroa.i.m@opendeusto.es>
2026-03-23 14:13:51 +0100

    fix: cambiar import

--- src/main/java/com/sergitxin/flexilearn/facade/AdminController.java
+++ src/main/java/com/sergitxin/flexilearn/facade/AdminController.java
@@ -1,5 +1,8 @@
 package com.sergitxin.flexilearn.facade;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,10 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sergitxin.flexilearn.dto.DeleteRequestDTO;
+import com.sergitxin.flexilearn.dto.UsuarioDTO;
+import com.sergitxin.flexilearn.entity.Usuario;
 import com.sergitxin.flexilearn.service.AdminService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -27,14 +32,31 @@ public class AdminController {
     }
 
     @Operation(summary = "Eliminar usuario", description = "Comprobar si es admin para poder eliminar usuarios")
-    @PostMapping("/deleteUsers")
+    @PostMapping("/deleteUser")
     public ResponseEntity<Integer> deleteUser(@RequestBody DeleteRequestDTO request){
+        System.out.println(request.getNombreUsuario());
+        System.out.println(request.getToken());
        boolean eliminado = adminService.eliminarUsuario(request.getToken(), request.getNombreUsuario());
        if (eliminado) {
         return ResponseEntity.ok(1);
        }
        return ResponseEntity.ok(0); 
     }
-}
 
+    @Operation(summary = "Usuarios", description = "Obtiene todos los usuarios")
+    @PostMapping("/users")
+    public ResponseEntity<List<UsuarioDTO>> getAllUsers(){
+        List<Usuario> usuarios = adminService.getAllUsers();
+        List<UsuarioDTO> usuariosDTO = new ArrayList<>();
+        for (Usuario usuario : usuarios) {
+            UsuarioDTO usuarioDTO = new UsuarioDTO();
+            usuarioDTO.setEmail(usuario.getEmail());
+            usuarioDTO.setEsAdmin(usuario.getEsAdmin());
+            usuarioDTO.setId(usuario.getId());
+            usuarioDTO.setNombre(usuario.getNombre());
+            usuariosDTO.add(usuarioDTO);
+        }
+        return ResponseEntity.ok(usuariosDTO);
+    }
+}
 
